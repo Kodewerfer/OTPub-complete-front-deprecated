06017e553e017e9b3ce046be956373d5e68e55b4
Alan Li <lijleen@gmail.com>
2017-05-02 11:39:33 +0800

    change img/sing-page to single-page, change minor css on powerBi

--- src/single-pages/powerBi.js
+++ src/single-pages/powerBi.js
@@ -4,11 +4,16 @@ require('videojs/video-js.css')
 require('videojs-sublime-skin/dist/videojs-sublime-skin.min.css')
 
 $(document).ready(function () {
+
   $('.flush.f-2').on('click', function () {
     if ($(this).hasClass('active')) {
       return;
     }
     $(this).addClass('active')
     vjs('specPlayer').play();
+  })
+
+  vjs('specPlayer').on('ended', function () {
+    $('.flush.f-2').removeClass('active')
   })
 })
